c16cb1637b9f9b3e16ab615c32a9ead702a5c73c
vddCore <vddcore@outlook.com>
2023-01-12 10:45:00 +0100

    fix

---
+++ Hash/Algorithms/Miscellaneous/Adler32.cs
@@ -0,0 +1,23 @@
+﻿namespace Arc.Hash.Algorithms.Miscellaneous
+{
+    public class Adler32 : IHash<uint>
+    {
+        private const int Mod = 65521;
+
+        internal Adler32()
+        {
+        }
+        
+        public uint Calculate(byte[] data)
+        {
+            uint c0 = 1, c1 = 0;
+            
+            foreach (var c in data) {
+                c0 = (c1 + c) % Mod;
+                c1 = (c1 + c0) % Mod;
+            }
+            
+            return (c1 << 16) | c0;
+        }
+    }
+}
